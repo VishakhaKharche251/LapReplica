87146bc053e3b57fa20b0178115c26e39d5efbc9
Vishakha Kharche <129781556+VishakhaKharche251@users.noreply.github.com>
2024-02-11 17:28:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Background from './components/Background';
+import './App.css';
+import Laptop from './components/Laptop';
+
+function App() {
+  return (
+    <>
+    <Background />
+    <Laptop />
+    </>
+  );
+}
+
+export default App;
